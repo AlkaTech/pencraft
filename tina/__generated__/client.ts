8f5f2c4742130b9168efaa5835fa35da4df9c0d7
John Onaya <37826079+AlkaTech@users.noreply.github.com>
2025-07-31 01:38:34 +0300

    Add files via upload

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: 'C:/Users/ONAYA/Desktop/CampaignCraft/pencraft/tina/__generated__/.cache/1753914964457', url: 'https://content.tinajs.io/1.6/content/a3a89282-4d60-46bc-932e-1135d200c729/github/main', token: '4030372dddc594c6edfdec37e11d93969511d30c', queries,  });
+export default client;
+  
