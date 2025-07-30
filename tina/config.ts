3122e018d198a6e79efe549984f7a229d2688387
John Onaya <37826079+AlkaTech@users.noreply.github.com>
2025-07-31 02:54:51 +0300

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -9,8 +9,8 @@ const branch =
 
 export default defineConfig({
 //...
-  token:  process.env.TINA_TOKEN, // This should match the value in your .env file
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID // This should match the value in your .env file
+  token:  '4030372dddc594c6edfdec37e11d93969511d30c' // This should match the value in your .env file
+  clientId: 'a3a89282-4d60-46bc-932e-1135d200c729', // This should match the value in your .env file
   branch,
   schema: {
     collections: [
