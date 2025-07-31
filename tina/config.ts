7ae7fbbc852ddeef54250894f427d1dc494e3a3e
John Onaya <37826079+AlkaTech@users.noreply.github.com>
2025-07-31 03:51:39 +0300

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -1,24 +1,16 @@
 import { defineConfig } from "tinacms";
 
-// Your hosting provider likely exposes this as an environment variable
-const branch =
+const branch = 
   process.env.NEXT_PUBLIC_TINA_BRANCH ||
   process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
   "main";
 
 export default defineConfig({
-//...
-  token:  '4030372dddc594c6edfdec37e11d93969511d30c' // This should match the value in your .env file
-  clientId: 'a3a89282-4d60-46bc-932e-1135d200c729', // This should match the value in your .env file
   branch,
-  schema: {
-    collections: [
-    //...
-    // See https://tina.io/docs/schema/ for more info about "collections"
-  ]}
-})
-
+  clientId: 'a3a89282-4d60-46bc-932e-1135d200c729', // From your .env
+  token: '4030372dddc594c6edfdec37e11d93969511d30c', // From your .env
+  
   build: {
     outputFolder: "admin",
     publicFolder: "static",
@@ -29,7 +21,6 @@ export default defineConfig({
       publicFolder: "static",
     },
   },
-  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
       {
@@ -53,5 +44,5 @@ export default defineConfig({
         ],
       },
     ],
-  },
+  }
 });
